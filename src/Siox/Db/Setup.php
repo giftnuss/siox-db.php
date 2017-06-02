5ed2e9a673a97c115ab64abb1d77484663adaebd
Sebastian B. Knapp <news@young-workers.de>
2017-06-02 07:48:01 +0200

    start db migration

--- src/Siox/Db/Setup.php
+++ src/Siox/Db/Setup.php
@@ -18,4 +18,17 @@ class Setup
             $db->sql()->createTable($table);
         }
     }
+
+    public function update($schema)
+    {
+        $db = $this->db;
+        foreach ($schema->getTables() as $table) {
+            try {
+                $db->sql()->countRows($table);
+            }
+            catch(\Exception $e) {
+                $db->sql()->createTable($table);
+            }
+        }
+    }
 }
